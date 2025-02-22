7e7882a9d2a832d2c22fa1a099a6e52a3479592c
hehooisbored <grantcrolon@gmail.com>
2025-02-21 19:32:58 -0600

    Cata - Talent and Glyph UI
    Skinned for talents and glyph UI in 4.4.2

--- ShestakUI/Modules/Skins/Blizzard_Cata/Talent.lua
+++ ShestakUI/Modules/Skins/Blizzard_Cata/Talent.lua
@@ -7,8 +7,8 @@ if C.skins.blizzard_frames ~= true then return end
 local function LoadSkin()
 	_G["PlayerTalentFrame"]:StripTextures()
 	_G["PlayerTalentFrame"]:CreateBackdrop("Transparent")
-	_G["PlayerTalentFrame"].backdrop:SetPoint("TOPLEFT", 10, -12)
-	_G["PlayerTalentFrame"].backdrop:SetPoint("BOTTOMRIGHT", -32, 76)
+	_G["PlayerTalentFrame"].backdrop:SetPoint("TOPLEFT", 0, 0)
+	_G["PlayerTalentFrame"].backdrop:SetPoint("BOTTOMRIGHT", -1, 2)
 
 	_G["PlayerTalentFramePortrait"]:Hide()
 
@@ -17,81 +17,83 @@ local function LoadSkin()
 	_G["PlayerTalentFrameTitleText"]:ClearAllPoints()
 	_G["PlayerTalentFrameTitleText"]:SetPoint("TOP", _G["PlayerTalentFrame"].backdrop, "TOP", 0, -6)
 
-	_G["PlayerTalentFrameSpentPointsText"]:ClearAllPoints()
-	_G["PlayerTalentFrameSpentPointsText"]:SetPoint("LEFT", _G["PlayerTalentFramePointsBar"], "LEFT", 8, -2)
-
-	_G["PlayerTalentFrameTalentPointsText"]:ClearAllPoints()
-	_G["PlayerTalentFrameTalentPointsText"]:SetPoint("RIGHT", _G["PlayerTalentFramePointsBar"], "RIGHT", 0, 0)
-
-	if _G["PlayerTalentFrameRoleButton"] then
-		_G["PlayerTalentFrameRoleButton"]:ClearAllPoints()
-		_G["PlayerTalentFrameRoleButton"]:SetPoint("TOPRIGHT", _G["PlayerTalentFrameScrollFrame"], "TOPRIGHT", 0, -6)
+	local function HandleTabs()
+		local lastTab
+		for index, tab in next, TalentTabs do
+			if index ~= 2 or HasPetUI() then
+				tab:ClearAllPoints()
+	
+				if index == 1 then
+					tab:Point('TOPLEFT', _G.PlayerTalentFrame, 'BOTTOMLEFT', -10, 0)
+				else
+					tab:Point('TOPLEFT', lastTab, 'TOPRIGHT', -19, 0)
+				end
+	
+				lastTab = tab
+			end
+		end
 	end
 
-	_G["PlayerTalentFrameTab1"]:ClearAllPoints()
-	_G["PlayerTalentFrameTab1"]:SetPoint("TOPLEFT", _G["PlayerTalentFrame"].backdrop, "BOTTOMLEFT", 2, -2)
+	for i = 1, MAX_TALENT_TABS do
+		local panelName = 'PlayerTalentFramePanel'..i
+		local panel = _G[panelName]
+		panel:StripTextures()
+		panel:CreateBackdrop("Transparent")
+		panel.backdrop:SetPoint("TOPLEFT", 0, 0)
+		panel.backdrop:SetPoint("BOTTOMRIGHT", 0, 0)
+		panel.SelectTreeButton:SkinButton()
+		-- Skin Talent Summary Panel
+		panel.Summary:StripTextures()
+		panel.Summary:CreateBackdrop()
+
+		-- Skin Talent Tree Headers
+		panel.HeaderIcon:StripTextures()
+		panel.HeaderIcon.Icon:SkinIcon()
+		panel.HeaderIcon.PointsSpent:SetPoint("LEFT", panel.RoleIcon2, "LEFT", -15, 0)
+		panel.HeaderIcon.PointsSpent:SetFont(C.media.pixel_font, C.media.pixel_font_size, C.media.pixel_font_style)
+		
+		-- Skin Talent Icons
+		for j = 1, MAX_NUM_TALENTS do
+			local talentNum = panelName.."Talent"..j
+			local talent = _G[talentNum]
+	
+			if talent then
+			talent:StripTextures()
+			talent:SetTemplate("Default")
+			talent:StyleButton()
+
+			talent.icon:SetInside()
+			talent.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+			talent.icon:SetDrawLayer("ARTWORK")
+
+			talent.Rank:SetFont(C.media.pixel_font, C.media.pixel_font_size, C.media.pixel_font_style)
+			end
+		end
+	end
+
+	-- Skin some buttons
+	_G["PlayerTalentFrameToggleSummariesButton"]:SkinButton()
+	_G["PlayerTalentFrameActivateButton"]:SkinButton()
+	_G["PlayerTalentFrameResetButton"]:SkinButton()
+	_G["PlayerTalentFrameLearnButton"]:SkinButton()
 
 	for i = 1, 5 do
 		T.SkinTab(_G["PlayerTalentFrameTab"..i])
 	end
 
-	_G["PlayerTalentFrameScrollFrame"]:StripTextures()
-	_G["PlayerTalentFrameScrollFrame"]:CreateBackdrop("Default")
-	_G["PlayerTalentFrameScrollFrame"].backdrop:SetPoint("TOPLEFT", -1, 1)
-	_G["PlayerTalentFrameScrollFrame"].backdrop:SetPoint("BOTTOMRIGHT", 6, -1)
-
-	T.SkinScrollBar(_G["PlayerTalentFrameScrollFrameScrollBar"])
-	_G["PlayerTalentFrameScrollFrameScrollBar"]:SetPoint("TOPLEFT", _G["PlayerTalentFrameScrollFrame"], "TOPRIGHT", 10, -16)
-
-	for i = 1, MAX_NUM_TALENTS do
-		local talent = _G["PlayerTalentFrameTalent"..i]
-		local icon = _G["PlayerTalentFrameTalent"..i.."IconTexture"]
-		local rank = _G["PlayerTalentFrameTalent"..i.."Rank"]
-
-		if talent then
-			talent:StripTextures()
-			talent:SetTemplate("Default")
-			talent:StyleButton()
-
-			icon:SetInside()
-			icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-			icon:SetDrawLayer("ARTWORK")
-
-			rank:SetFont(C.media.pixel_font, C.media.pixel_font_size, C.media.pixel_font_style)
+	for i = 1, 3 do
+		local tab = _G["PlayerSpecTab"..i]
+		if tab then
+			tab:GetRegions():SetSize(0.1, 0.1)
+			tab:GetNormalTexture():SetTexCoord(0.1, 0.9, 0.1, 0.9)
+			tab:GetNormalTexture():ClearAllPoints()
+			tab:GetNormalTexture():SetPoint("TOPLEFT", 2, -2)
+			tab:GetNormalTexture():SetPoint("BOTTOMRIGHT", -2, 2)
+			tab:CreateBackdrop("Default")
+			tab.backdrop:SetAllPoints()
+			tab:StyleButton(true)
 		end
 	end
-
-	for i = 1, MAX_TALENT_TABS do
-		local tab = _G["PlayerSpecTab"..i]
-		tab:GetRegions():Hide()
-
-		tab:SetTemplate("Default")
-		tab:StyleButton(true)
-
-		tab:GetNormalTexture():SetInside()
-		tab:GetNormalTexture():SetTexCoord(0.1, 0.9, 0.1, 0.9)
-	end
-
-	_G["PlayerTalentFrameStatusFrame"]:SetPoint("TOP", 8, -30)
-	_G["PlayerTalentFrameStatusFrame"]:StripTextures()
-
-	_G["PlayerTalentFrameActivateButton"]:SetPoint("TOP", -10, -40)
-	_G["PlayerTalentFrameActivateButton"]:SkinButton()
-
-	_G["PlayerTalentFrameResetButton"]:SetPoint("RIGHT", -2, -1)
-	_G["PlayerTalentFrameResetButton"]:SkinButton()
-
-	_G["PlayerTalentFrameLearnButton"]:SetPoint("RIGHT", _G["PlayerTalentFrameResetButton"], "LEFT", -3, 0)
-	_G["PlayerTalentFrameLearnButton"]:SkinButton()
-
-	_G["PlayerSpecTab1"]:SetPoint("TOPLEFT", _G["PlayerTalentFrame"], "TOPRIGHT", -33, -65)
-	_G["PlayerSpecTab1"].ClearAllPoints = T.dummy
-	_G["PlayerSpecTab1"].SetPoint = T.dummy
-
-	_G["PlayerTalentFramePreviewBar"]:StripTextures()
-	_G["PlayerTalentFramePreviewBarFiller"]:StripTextures()
-
-	_G["PlayerTalentFramePointsBar"]:StripTextures()
 
 	local f = CreateFrame("Frame")
 	f:RegisterEvent("CHARACTER_POINTS_CHANGED")
